d003dae79486b8b3085e4fb7554716c1fb937333
Albedo-Parona <104846975+Albedo-Parona@users.noreply.github.com>
2025-03-29 19:08:45 +0200

    Add files via upload

---
+++ Selemium_task/Elem_ident.py
@@ -0,0 +1,6 @@
+#Elements identificators
+ProfessionCards_content = 'ProfessionCard_content__mPiVi'                       #ClassName
+ProfessionCards_links = '//*[@id="all-courses"]/div/div[2]/a'                   #XPath
+CourseModulesList_modules_list = 'CourseModulesList_modulesList__C86yL'         #ClassName
+Modules_classname = 'color-dark-blue'                                           #ClassName
+Theme_window = '//*[@id="course-program"]/div/div[2]/div[2]/div[1]/div[1]/p[1]' #XPath
---
+++ Selemium_task/Read_me.txt
@@ -0,0 +1,1 @@
+The task was performed in PyCHarm CE
